ac87b1d5166874238119f92e19cbfda4bc30bb06
juliemachiels <j.machiels@innervate.nl>
2021-04-01 17:05:57 +0200

    Changed prompt to select_account

--- simple_auth/lib/src/providers/azureAD.dart
+++ simple_auth/lib/src/providers/azureAD.dart
@@ -70,7 +70,7 @@ class AzureADAuthenticator extends OAuthAuthenticator {
     if (!useClientSecret && map.containsKey("client_secret")) {
       map.remove("client_secret");
     }
-    map["prompt"] = "login";
+    map["prompt"] = "select_account";
     map["resource"] = resource;
     return map;
   }
